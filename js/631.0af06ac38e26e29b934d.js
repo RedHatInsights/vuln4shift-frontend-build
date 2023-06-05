7453d8332734c3cbbe9f2e5b8c51cc6976398e23
Travis CI User <travis@example.org>
2023-06-05 21:05:18 +0000

    Test dynamic filters on Cluster list page

---
+++ js/631.0af06ac38e26e29b934d.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkocp_vulnerability_frontend=self.webpackChunkocp_vulnerability_frontend||[]).push([[631],{28412:(e,t,n)=>{n.r(t),n.d(t,{default:()=>i});var r=n(93264),a=n.n(r),l=n(16530),o=n(39704),c=n(21535),u=n(87999),s=n(35240);const i=function(){return a().createElement(o.zt,{store:(0,c.S)().getStore()},a().createElement(l.BrowserRouter,{basename:(0,s.eb)(window.location.pathname,3)},a().createElement(u.Z,null)))}}}]);
+//# sourceMappingURL=../sourcemaps/631.21fb1edccd6a349061f9e8c714c4f108.js.map
